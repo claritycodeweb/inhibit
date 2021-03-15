482b1f2c2892f08ac9108be75126885113348781
Rafal Pisarczyk <rafal.pisarczyk@lookmove.ch>
2021-03-15 13:17:31 +0100

    Updates

---
+++ precache-manifest.0ffcc7312abf6c848eb357101425e908.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d50e68ebfe8d277a6c29113bdbd5cc5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "aabc1cd59bf121316c44",
+    "url": "./static/css/main.c478715c.chunk.css"
+  },
+  {
+    "revision": "1885d4bdb671d7435408",
+    "url": "./static/js/2.31059e14.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.31059e14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aabc1cd59bf121316c44",
+    "url": "./static/js/main.5d4a47a8.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.5d4a47a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2438f933d5d46d4ed583",
+    "url": "./static/js/runtime-main.88be6064.js"
+  }
+]);
